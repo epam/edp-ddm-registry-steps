924f2cfd0f0ee4d7495c8873653471541271df50
Kateryna_Rozhkovska <kateryna_rozhkovska@epam.com>
2022-10-03 12:32:20 +0300

    [EDP-DDM-00000] Add steps for random value
    Change-Id: I59b14503f9a64033bf8ff72eb78225d15c3a5376

--- src/main/java/platform/qa/base/convertors/ContextConvertor.java
+++ src/main/java/platform/qa/base/convertors/ContextConvertor.java
@@ -89,10 +89,9 @@ public class ContextConvertor {
     }
 
     public static HashMap<String, String> convertToRandomMapContext(Object context) {
-        if (context instanceof HashMap<?,?>){
-           return (HashMap<String,String>) context;
-        }
         var convertedContext = new HashMap<String, String>();
+        if (context instanceof HashMap){
+           return (HashMap<String, String>) context;}
         return convertedContext;
     }
 }
